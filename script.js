21d991747f79496efc5bbd26391b243957266993
Swaroop Patil <66289578+swarooppatilx@users.noreply.github.com>
2024-04-13 19:32:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function getWeather() {
     const pincode = document.getElementById('pincodeInput').value;
     const apiKey = '513ac8debfc21bad0d5e19eb15a4c8c3';
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${pincode}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${pincode}&appid=${apiKey}&units=metric`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            const cityName = data.name;
             const temperature = data.main.temp;
             const humidity = data.main.humidity;
-            const moisture = data.main.pressure;
+            const pressure = data.main.pressure;
 
-            document.getElementById('temperature').textContent = temperature;
-            document.getElementById('humidity').textContent = humidity;
-            document.getElementById('moisture').textContent = moisture;
+            document.getElementById('cityName').textContent = `City: ${cityName}`;
+            document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
+            document.getElementById('humidity').textContent = `Humidity: ${humidity}%`;
+            document.getElementById('pressure').textContent = `Pressure: ${pressure} hPa`;
         })
         .catch(error => {
             console.error('Error:', error);
